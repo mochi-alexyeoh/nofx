a21162d913596e9f0a7b15cb74d2eb8c00865594
Syamsoul Azrien <syamsoulazrien88@gmail.com>
2026-05-14 13:40:50 +0800

    [skip ci] feat: pass COMMIT_SHA to deployment script and use git reset to ensure synchronization with target commit

--- .github/workflows/scripts/deployment_vm.sh
+++ .github/workflows/scripts/deployment_vm.sh
@@ -5,5 +5,7 @@ cd $PROJECT_PATH
 echo "> Directory changed to: $(pwd)"
 
 # Build new image and start new containers
-sudo git pull
+# Fetch latest changes and reset to the specific commit
+sudo git fetch origin
+sudo git reset --hard $COMMIT_SHA
 sudo docker compose -f docker-compose.prod.yml up -d --build --remove-orphans
