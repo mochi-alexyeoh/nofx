ee8919cb2ab0e4109c197b71239930f49cf0ec9b
Syamsoul Azrien <syamsoulazrien88@gmail.com>
2026-05-14 12:24:49 +0800

    git pull before build new image

--- .github/workflows/scripts/deployment_vm.sh
+++ .github/workflows/scripts/deployment_vm.sh
@@ -5,4 +5,5 @@ cd $PROJECT_PATH
 echo "> Directory changed to: $(pwd)"
 
 # Build new image and start new containers
+sudo git pull
 sudo docker compose -f docker-compose.prod.yml up -d --build --remove-orphans
